d59a4ce9fd8950ff66f614168e9d338d8762458d
IKholopov <kholopov96@gmail.com>
2017-04-24 03:23:01 +0300

    Publish

---
+++ app/src/androidTest/java/com/supdev/igor/mysunshine/TestFetchWeatherTask.java
@@ -0,0 +1,20 @@
+package com.supdev.igor.mysunshine;
+
+/**
+ * Created by igor on 7/15/16.
+ */
+
+import android.test.AndroidTestCase;
+
+public class TestFetchWeatherTask extends AndroidTestCase{
+    static final String ADD_LOCATION_SETTING = "Sunnydale, CA";
+    static final String ADD_LOCATION_CITY = "Sunnydale";
+    static final double ADD_LOCATION_LAT = 34.425833;
+    static final double ADD_LOCATION_LON = -119.714167;
+
+    /*
+        Students: uncomment testAddLocation after you have written the AddLocation function.
+        This test will only run on API level 11 and higher because of a requirement in the
+        content provider.
+     */
+}
